876ef44874d8e3c4638cc327ec4463ed35a44fb9
Mainak Mondal <mainak.mondal33@gmail.com>
2019-11-27 16:29:00 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path=require('path')  
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+
+const app=express()
+
+//Define path for express config
+const publicPath=path.join(__dirname,'../public')
+const viewPath=path.join(__dirname,'../template/views')
+const partialPath=path.join(__dirname,'../template/partials')
+
+//Setup handlebar engine and views location
+app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialPath)
+
+//Setup static directory to serve
+app.use(express.static(publicPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Mainak'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'Mainak'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpMessege:'This is some helpfull text',
+        title:'Help',
+        name:'Mainak'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+        return res.send({
+            error:'Address is not provided'
+        });
+    
+        geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+            if(error){
+                return res.send({error})
+            }
+            forecast(latitude,longitude,(error,forecastData)=>{
+                if(error){
+                    return res.send({error})
+                }
+                res.send({
+                    forecast:forecastData,
+                    location,
+                    address:req.query.address
+                })
+
+            })
+        })
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search)
+        return res.send({
+            error:'Search parameter is not provided'
+        });
+    console.log(req.query.search);
+    res.send({
+        product:[]
+    });
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        name:'Mainak',
+        title:'404',
+        errorMessage:'Article is not found'
+        
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        name:'Mainak',
+        title:'404',
+        errorMessage:'Page not found'
+        
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,14 @@
+const request=require('request')
+const forecast=(latitude,longitude,callback)=>{
+    const url='https://api.darksky.net/forecast/129b68f5420ba483474536854bddcdb6/'+ latitude+','+longitude+'?lang=es'
+    request({url,json:true},(error,{body}={})=>{
+        if(error){
+            callback('Unable to connect forecast services',undefined);
+        }else if(body.error){
+            callback('Unable to find location',undefined)
+        }else{
+            callback(undefined,'This is '+body.currently.temperature+' degree out. There is '+body.currently.precipProbability+'% chance of Rain.')
+        }
+    })
+}
+module.exports=forecast`
